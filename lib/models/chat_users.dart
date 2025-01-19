8699b842e3a7caa912c70d6b071c73e98d36306e
surendar-05 <surendarrockzz7@gmail.com>
2025-01-19 19:08:37 +0530

    feat: Implemented the chat users

---
+++ lib/models/chat_users.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+
+class ChatUsers {
+  static types.User currentUser = types.User(
+    id: 'karthidreamr',
+    firstName: 'KarthiDreamr',
+  );
+
+  static types.User aiUser = types.User(
+    id: 'gemini-ai',
+    firstName: 'Gemini',
+  );
+}
